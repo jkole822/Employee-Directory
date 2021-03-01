fafa0e9120965277afaa90958cbc4a6967f4453a
Kole Gasior <jkole822@gmail.com>
2021-03-01 16:04:08 -0500

    create html skeleton

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TableRow = () => {
-	return <div>Table row works</div>;
+	return (
+		<tr>
+			<td>
+				<img src="" className="uk-preserve-width uk-border-circle" alt="" />
+			</td>
+			<td>Name</td>
+			<td>DOB</td>
+			<td>Gender</td>
+			<td>Location</td>
+			<td>Email</td>
+			<td>Username</td>
+		</tr>
+	);
 };
 
 export default TableRow;
