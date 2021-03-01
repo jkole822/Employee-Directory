d0eec396575f05fccc05512429c6a1fc30e366fa
Kole Gasior <jkole822@gmail.com>
2021-03-01 16:47:00 -0500

    fix typo, remove gender opt

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,18 +2,17 @@ import React from "react";
 
 const Form = () => {
 	return (
-		<form class="uk-form-stacked">
-			<div class="uk-margin">
-				<input class="uk-input" placeholder="Search" />
+		<form className="uk-form-stacked">
+			<div className="uk-margin">
+				<input className="uk-input" placeholder="Search" />
 			</div>
-			<div class="uk-margin">
-				<select class="uk-select">
-					<option selected disabled>
+			<div className="uk-margin">
+				<select defaultValue="" className="uk-select">
+					<option value="" disabled>
 						Filter by:
 					</option>
 					<option>Name</option>
 					<option>Age</option>
-					<option>Gender</option>
 					<option>Location</option>
 					<option>Email</option>
 					<option>Username</option>
