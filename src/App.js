26334aaa63fdf6e943c0a283c9d2d90a411083f0
Kole Gasior <jkole822@gmail.com>
2021-03-01 18:09:37 -0500

    declare filterOption and filterText state; pass props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import Form from "./components/Form";
 import Table from "./components/Table";
 
-function App() {
-	return (
-		<div className="uk-container">
-			<Form />
-			<Table />
-		</div>
-	);
+class App extends React.Component {
+	state = {
+		filterOption: "",
+		filterText: "",
+	};
+
+	handleSubmit = (filterOption, filterText) => {
+		this.setState({ filterOption, filterText });
+	};
+
+	render() {
+		return (
+			<div className="uk-container">
+				<Form filterSubmit={this.handleSubmit} />
+				<Table
+					filterText={this.state.filterText}
+					filterOption={this.state.filterOption}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
