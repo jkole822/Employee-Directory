c3980eeb922336ea9fdeb5e21e79c81e6f90f7be
Kole Gasior <jkole822@gmail.com>
2021-03-01 18:10:51 -0500

    handle form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,53 @@
 import React from "react";
 
-const Form = () => {
-	return (
-		<form className="uk-form-stacked">
-			<div className="uk-margin">
-				<input className="uk-input" placeholder="Search" />
-			</div>
-			<div className="uk-margin">
-				<select defaultValue="" className="uk-select">
-					<option value="" disabled>
-						Filter by:
-					</option>
-					<option>Name</option>
-					<option>Age</option>
-					<option>Location</option>
-					<option>Email</option>
-					<option>Username</option>
-				</select>
-			</div>
-		</form>
-	);
-};
+class Form extends React.Component {
+	state = {
+		filterOption: "",
+		filterText: "",
+	};
+
+	handleSubmit = event => {
+		event.preventDefault();
+
+		this.props.filterSubmit(this.state.filterOption, this.state.filterText);
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit} className="uk-form-stacked">
+				<div className="uk-margin">
+					<input
+						autoCapitalize="off"
+						autoComplete="off"
+						value={this.state.filterText}
+						onChange={event =>
+							this.setState({ filterText: event.target.value })
+						}
+						className="uk-input"
+						placeholder="Search"
+					/>
+				</div>
+				<div className="uk-margin">
+					<select
+						onChange={event =>
+							this.setState({ filterOption: event.target.value })
+						}
+						defaultValue=""
+						className="uk-select"
+					>
+						<option value="" disabled>
+							Filter by:
+						</option>
+						<option value="name">Name</option>
+						<option value="age">Age</option>
+						<option value="location">Location</option>
+						<option value="email">Email</option>
+						<option value="username">Username</option>
+					</select>
+				</div>
+			</form>
+		);
+	}
+}
 
 export default Form;
