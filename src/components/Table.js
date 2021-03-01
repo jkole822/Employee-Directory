e8161a04452e6d7fc2f85ddc78b0c2cba4cf4694
Kole Gasior <jkole822@gmail.com>
2021-03-01 16:05:20 -0500

    create html skeleton; store api res in state

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,13 +1,38 @@
 import React from "react";
+import axios from "axios";
+
 import TableRow from "./TableRow";
 
-const Table = () => {
-	return (
-		<div>
-			<p>Table works</p>
-			<TableRow />
-		</div>
-	);
-};
+class Table extends React.Component {
+	state = {
+		employees: [],
+	};
+
+	async componentDidMount() {
+		const res = await axios.get("https://randomuser.me/api/?results=500");
+		this.setState({ employees: [...res.data.results] });
+	}
+
+	render() {
+		return (
+			<div>
+				<table class="uk-table uk-table-striped uk-table-justify">
+					<thead>
+						<tr>
+							<th class="uk-table-shrink"></th>
+							<th>Name</th>
+							<th>DOB</th>
+							<th>Gender</th>
+							<th>Location</th>
+							<th>Email</th>
+							<th>Username</th>
+						</tr>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</div>
+		);
+	}
+}
 
 export default Table;
