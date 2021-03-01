a6560263ec7b9aa2b42c69c3ad209a84bf46b4e2
Kole Gasior <jkole822@gmail.com>
2021-03-01 16:04:36 -0500

    create html skeleton

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Form = () => {
-	return <div>Form works</div>;
+	return (
+		<form class="uk-form-stacked">
+			<div class="uk-margin">
+				<input class="uk-input" placeholder="Search" />
+			</div>
+			<div class="uk-margin">
+				<select class="uk-select">
+					<option selected disabled>
+						Filter by:
+					</option>
+					<option>Name</option>
+					<option>Age</option>
+					<option>Gender</option>
+					<option>Location</option>
+					<option>Email</option>
+					<option>Username</option>
+				</select>
+			</div>
+		</form>
+	);
 };
 
 export default Form;
