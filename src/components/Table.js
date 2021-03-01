15b4720c7cc6ef39d8f430f1c0c8dba718369ec5
Kole Gasior <jkole822@gmail.com>
2021-03-01 16:50:59 -0500

    add props for TableRow comp, declare employeeList for state, remove gender col

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,30 +5,54 @@ import TableRow from "./TableRow";
 
 class Table extends React.Component {
 	state = {
+		employeeList: [],
 		employees: [],
 	};
 
 	async componentDidMount() {
-		const res = await axios.get("https://randomuser.me/api/?results=500");
-		this.setState({ employees: [...res.data.results] });
+		const res = await axios.get(
+			"https://randomuser.me/api/?results=500&nat=us"
+		);
+		this.setState({
+			employeeList: res.data.results,
+			employees: res.data.results,
+		});
 	}
+
+	renderRows = () => {
+		return this.state.employees.map(employee => {
+			return (
+				<TableRow
+					key={employee.login.sha256}
+					image={employee.picture.thumbnail}
+					firstName={employee.name.first}
+					lastName={employee.name.last}
+					state={employee.location.state}
+					city={employee.location.city}
+					dob={employee.dob.date}
+					age={employee.dob.age}
+					email={employee.email}
+					username={employee.login.username}
+				/>
+			);
+		});
+	};
 
 	render() {
 		return (
 			<div>
-				<table class="uk-table uk-table-striped uk-table-justify">
+				<table className="uk-table uk-table-striped uk-table-justify">
 					<thead>
 						<tr>
-							<th class="uk-table-shrink"></th>
+							<th className="uk-table-shrink"></th>
 							<th>Name</th>
 							<th>DOB</th>
-							<th>Gender</th>
 							<th>Location</th>
 							<th>Email</th>
 							<th>Username</th>
 						</tr>
 					</thead>
-					<tbody></tbody>
+					<tbody>{this.renderRows()}</tbody>
 				</table>
 			</div>
 		);
