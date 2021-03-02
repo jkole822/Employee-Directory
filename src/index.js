bb6fc9a8767eec79fc1f901355797a825c35b68a
Kole Gasior <jkole822@gmail.com>
2021-03-01 20:15:29 -0500

    import and use UIkit icons

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "uikit/dist/css/uikit.min.css";
-import "uikit/dist/js/uikit.min.js";
-import "uikit/dist/js/uikit-icons.min.js";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+UIkit.use(Icons);
 
 ReactDOM.render(
 	<React.StrictMode>
